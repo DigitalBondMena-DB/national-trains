90f202dd59df56de605665b0a5a308d509d1b858
ahmedWessamTest <ahmedwessam202@gmail.com>
2026-02-04 10:54:43 +0200

    minifi js

--- js/function.js
+++ js/function.js
@@ -1,1 +1,91 @@
-!function(e){"use strict";var t=e(window);e("body");if(t.on("load",(function(){e(".preloader").fadeOut(600)})),e(".active-sticky-header").length){function l(){e("header.main-header").css("height",e("header .header-sticky").outerHeight())}t.on("resize",(function(){l()})),e(window).on("scroll",(function(){var t=e(window).scrollTop();l();var a=e("header .header-sticky").outerHeight();e("header .header-sticky").toggleClass("hide",t>a+100),e("header .header-sticky").toggleClass("active",t>600)}))}e("#menu").slicknav({label:"",prependTo:".responsive-menu"}),e("a[href='#top']").length&&e("a[href='#top']").click((function(){return e("html, body").animate({scrollTop:0},"slow"),!1}));new Swiper(".hero-slider-layout .swiper",{slidesPerView:1,speed:1e3,spaceBetween:0,loop:!0,autoplay:{delay:4e3},pagination:{el:".swiper-pagination",clickable:!0}});if(e(".testimonial-slider").length){new Swiper(".testimonial-slider .swiper",{slidesPerView:1,speed:1e3,spaceBetween:30,loop:!0,autoplay:{delay:3e3},pagination:{el:".swiper-pagination",clickable:!0},breakpoints:{768:{slidesPerView:2},991:{slidesPerView:3}}})}if(e(".service-images-slider").length){new Swiper(".service-images-slider .swiper",{slidesPerView:1,speed:1e3,spaceBetween:10,loop:!0,centeredSlides:!0,autoplay:{delay:5e3},navigation:{nextEl:".swiper-arrow-next",prevEl:".swiper-arrow-prev"}})}if(e(".counter").length&&e(".counter").counterUp({delay:6,time:3e3}),e(".reveal").length){gsap.registerPlugin(ScrollTrigger),document.querySelectorAll(".reveal").forEach((e=>{let t=e.querySelector("img"),a=gsap.timeline({scrollTrigger:{trigger:e,toggleActions:"play none none none"}});a.set(e,{autoAlpha:1}),a.from(e,1,{xPercent:-100,ease:Power2.out}),a.from(t,1,{xPercent:100,scale:1,delay:-1,ease:Power2.out})}))}if(e(".text-anime-style-1").length){let s=.05,o=.5;document.querySelectorAll(".text-anime-style-1").forEach((e=>{let t=new SplitText(e,{type:"chars, words"});gsap.from(t.words,{duration:1,delay:o,x:20,autoAlpha:0,stagger:s,scrollTrigger:{trigger:e,start:"top 85%"}})}))}if(e(".text-anime-style-2").length){let n=.05,p=20,c=.5,g="power2.out";document.querySelectorAll(".text-anime-style-2").forEach((e=>{let t=new SplitText(e,{type:"chars, words"});gsap.from(t.chars,{duration:1,delay:c,x:p,autoAlpha:0,stagger:n,ease:g,scrollTrigger:{trigger:e,start:"top 85%"}})}))}if(e(".text-anime-style-3").length){document.querySelectorAll(".text-anime-style-3").forEach((e=>{e.animation&&(e.animation.progress(1).kill(),e.split.revert()),e.split=new SplitText(e,{type:"lines,words,chars",linesClass:"split-line"}),gsap.set(e,{perspective:400}),gsap.set(e.split.chars,{opacity:0,x:"50"}),e.animation=gsap.to(e.split.chars,{scrollTrigger:{trigger:e,start:"top 90%"},x:"0",y:"0",rotateX:"0",opacity:1,duration:1,ease:Back.easeOut,stagger:.02})}))}var a=e(".parallaxie");a.length&&t.width()>991&&t.width()>768&&a.parallaxie({speed:.55,offset:0}),e(".project-gallery-items").magnificPopup({delegate:"a",type:"image",closeOnContentClick:!1,closeBtnInside:!1,mainClass:"mfp-with-zoom",image:{verticalFit:!0},gallery:{enabled:!0},zoom:{enabled:!0,duration:300,opener:function(e){return e.find("img")}}});var r=e("#contactForm");function i(t,a){if(t)var r="h3 text-success";else r="h3 text-danger";e("#msgSubmit").removeClass().addClass(r).text(a)}r.validator({focus:!1}).on("submit",(function(t){var a,l,s,o,n;t.isDefaultPrevented()||(t.preventDefault(),a=e("#fullname").val(),l=e("#email").val(),s=e("#phone").val(),o=e("#subject").val(),n=e("#msg").val(),e.ajax({type:"POST",url:"form-process.php",data:"fullname="+a+"&name=&email="+l+"&phone="+s+"&subject="+o+"&message="+n,success:function(e){"success"==e?(r[0].reset(),i(!0,"Message Sent Successfully!")):i(!1,e)}}))})),(new WOW).init(),e(".popup-video").length&&e(".popup-video").magnificPopup({type:"iframe",mainClass:"mfp-fade",removalDelay:160,preloader:!1,fixedContentPos:!0})}(jQuery);
+!(function (e) {
+    "use strict";
+    var a = e(window);
+    e("body");
+    if (
+        (a.on("load", function () {
+            e(".preloader").fadeOut(600);
+        }),
+        e(".active-sticky-header").length)
+    ) {
+        function t() {
+            e("header.main-header").css("height", e("header .header-sticky").outerHeight());
+        }
+        a.on("resize", function () {
+            t();
+        }),
+            e(window).on("scroll", function () {
+                var a = e(window).scrollTop();
+                t();
+                var i = e("header .header-sticky").outerHeight();
+                e("header .header-sticky").toggleClass("hide", a > i + 100),
+                    e("header .header-sticky").toggleClass("active", a > 600);
+            });
+    }
+    new Swiper(".hero-slider-layout .swiper", {
+        slidesPerView: 1,
+        speed: 1e3,
+        spaceBetween: 0,
+        loop: !0,
+        autoplay: { delay: 4e3 },
+        pagination: { el: ".swiper-pagination", clickable: !0 },
+    });
+    if (e(".testimonial-slider").length) {
+        new Swiper(".testimonial-slider .swiper", {
+            slidesPerView: 1,
+            speed: 1e3,
+            spaceBetween: 30,
+            loop: !0,
+            autoplay: { delay: 3e3 },
+            pagination: { el: ".swiper-pagination", clickable: !0 },
+            breakpoints: { 768: { slidesPerView: 2 }, 991: { slidesPerView: 3 } },
+        });
+    }
+    if (e(".service-images-slider").length) {
+        new Swiper(".service-images-slider .swiper", {
+            slidesPerView: 1,
+            speed: 1e3,
+            spaceBetween: 10,
+            loop: !0,
+            centeredSlides: !0,
+            autoplay: { delay: 5e3 },
+            navigation: { nextEl: ".swiper-arrow-next", prevEl: ".swiper-arrow-prev" },
+        });
+    }
+    if ((e(".counter").length && e(".counter").counterUp({ delay: 6, time: 3e3 }), e(".reveal").length)) {
+        gsap.registerPlugin(ScrollTrigger),
+            document.querySelectorAll(".reveal").forEach((e) => {
+                let a = e.querySelector("img"),
+                    i = gsap.timeline({ scrollTrigger: { trigger: e, toggleActions: "play none none none" } });
+                i.set(e, { autoAlpha: 1 }),
+                    i.from(e, 1, { xPercent: -100, ease: Power2.out }),
+                    i.from(a, 1, { xPercent: 100, scale: 1, delay: -1, ease: Power2.out });
+            });
+    }
+    var i = e(".parallaxie");
+    i.length && a.width() > 991 && a.width() > 768 && i.parallaxie({ speed: 0.55, offset: 0 });
+    var r = e("#contactForm");
+    function s(a, i) {
+        if (a) var r = "h3 text-success";
+        else r = "h3 text-danger";
+        e("#msgSubmit").removeClass().addClass(r).text(i);
+    }
+    r.validator({ focus: !1 }).on("submit", function (a) {
+        var i, t, l, n, o;
+        a.isDefaultPrevented() ||
+            (a.preventDefault(),
+            (i = e("#fullname").val()),
+            (t = e("#email").val()),
+            (l = e("#phone").val()),
+            (n = e("#subject").val()),
+            (o = e("#msg").val()),
+            e.ajax({
+                type: "POST",
+                url: "form-process.php",
+                data: "fullname=" + i + "&name=&email=" + t + "&phone=" + l + "&subject=" + n + "&message=" + o,
+                success: function (e) {
+                    "success" == e ? (r[0].reset(), s(!0, "Message Sent Successfully!")) : s(!1, e);
+                },
+            }));
+    });
+})(jQuery);
